4c2d1ad0ec59aa82569422443f9c182cdca0f4a3
bucholga <buchatskaya_olga_112@mail.ru>
2021-08-24 12:00:56 +0300

    Words in song

--- src/main/java/streams/Main.java
+++ src/main/java/streams/Main.java
@@ -20,7 +20,7 @@ public class Main {
                     .count();
             System.out.println(count);
         } catch (IOException e){
-            e.printStackTrace();
+            e.printStackTrace();//
         }
     }
 }
